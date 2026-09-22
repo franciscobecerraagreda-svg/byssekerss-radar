8bc0c7ce3db379889ddf8f8e529a179323e50aac
franciscobecerraagreda-svg <franciscobecerraagreda@gmail.com>
2026-09-21 20:24:23 -0600

    Create foros.js

---
+++ api/foros.js
@@ -0,0 +1,5 @@
+export default async function handler(req,res){
+ return res.json({count: 1, items: [
+  {titulo: "CloudyNights - Anomalía", desc: "Objeto no identificado en foro", link: "https://cloudynights.com"}
+ ]})
+}
